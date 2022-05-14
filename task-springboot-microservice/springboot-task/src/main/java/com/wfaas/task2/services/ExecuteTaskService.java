9f6be85a3e121cb7a48bc52f6e8d26c8045b09bd
karthik-cm <karthikcm.77@gmail.com>
2022-05-13 21:22:13 -0700

    task 2 microservice commit

--- task-springboot-microservice/springboot-task/src/main/java/com/wfaas/task2/services/ExecuteTaskService.java
+++ task-springboot-microservice/springboot-task/src/main/java/com/wfaas/task2/services/ExecuteTaskService.java
@@ -1,51 +1,85 @@
 package com.wfaas.task2.services;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.client.RestTemplate;
 
 public class ExecuteTaskService implements Runnable {
 	
+	private static final String EMAIL_SUBJECT = "Workflow as a Service email : Successfully registered";
+	private static final String EMAIL_BODY = "You are receiving this email since you have just registered for the service!\n\n"
+											+ "Thanks and Regards\n"
+											+ "Workflow as a Services Team";
+	
+
+	
+	@Value("${spring.mail.username}") private String sender;
+	
+	private JavaMailSenderImpl javaMailSender;
 	private String workflowId;
 	private String taskId;
 	private JSONObject attributes;
 	
-	public ExecuteTaskService(String workflowId, String taskId, JSONObject attributes){
+	public ExecuteTaskService(JavaMailSenderImpl javaMailSender, String workflowId, String taskId, JSONObject attributes){
+		this.javaMailSender = javaMailSender;
 		this.workflowId = workflowId;
 		this.taskId = taskId;
 		this.attributes = attributes;
 	}
 	
 	
+	
 	@Override
 	public void run() {
-		executeTask();
-		sendResponseToScheduler(workflowId, taskId, attributes);
+		// Define and program your task here
+		sendEmail();
 	}
 	
-	public String executeTask() {
-		try{
-			Thread.sleep(5000);
+	
+	// Task - Send email 
+	private void sendEmail() {
+		System.out.println("Inside sendEmail() :::::: TriggerEmailService : "
+				+ "workflowId = "+workflowId +", taskId = "+taskId +", attributes = "+attributes);
+		
+		try {
+			String recipient = this.attributes.has("email") ? this.attributes.getString("email") : null;
+			
+			if(recipient != null && recipient.length() > 0) {
+				SimpleMailMessage mailMessage = new SimpleMailMessage();
+				
+				// set all the required attributes for the email
+				mailMessage.setFrom(sender);
+				mailMessage.setTo(recipient);
+				mailMessage.setSubject(EMAIL_SUBJECT);
+				mailMessage.setText(EMAIL_BODY);
+				
+				
+				// trigger email
+				javaMailSender.send(mailMessage);
+				System.out.println("Success: Email sent successfully!");
+				
+				
+				// send the response to Scheduler service
+				sendResponseToScheduler(this.workflowId, this.taskId, this.attributes);
+			}
+			else {
+				System.out.println("Error: Recipient cannot be empty!");
+			}
 		}
 		catch(Exception e) {
-			
+			System.out.println("Some error occurred in sendEmail() ::::: TriggerEmailService, "+e);
 		}
-		
-		System.out.println("<<::::: From Spring Boot Microservice task :::::>>");
-		
-		String output = "Hello, World 2";
-		System.out.println("Task #2 output - "+output);
-		
-		String response = "Task #2 is successfully completed!";
-		System.out.println(response);
-		return response;
 	}
 	
+	
 	public void sendResponseToScheduler(String workflowId, String taskId, JSONObject attributes) {
-		String URL = taskId +"-service:8080/completeTask/";
+		String URL = taskId +"-service:8080/completeTask/"; // check for this URL
 		
 		try {
 			HttpHeaders headers = new HttpHeaders();
@@ -56,7 +90,7 @@ public class ExecuteTaskService implements Runnable {
 			body.put("taskId", taskId);
 			body.put("attributes", attributes);
 			
-			body.put("status", "TASK_COMPLETED");
+			body.put("status", "COMPLETED");
 			body.put("msg", "Task #2 is successfully completed");
 			
 			
@@ -70,8 +104,8 @@ public class ExecuteTaskService implements Runnable {
 			System.out.println("Response = "+response);
 		}
 		catch(Exception e) {
-			System.out.println("Some error occurred in sendResponseToScheduler() ::::: ExecuteTaskService, "+e);
+			System.out.println("Some error occurred in sendResponseToScheduler() ::::: TriggerEmailService, "+e);
 		}
-		
 	}
+	
 }
