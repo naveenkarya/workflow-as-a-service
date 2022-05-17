2e2c3a2416b8682316398d027e5307218b91ffdf
Naveen Kumar <naveenkarya@github.com>
2022-05-16 22:41:14 -0700

    deployment from code - initial code

--- workflow-generator/src/main/java/edu/coen241/workflowgen/controller/WorkflowRestController.java
+++ workflow-generator/src/main/java/edu/coen241/workflowgen/controller/WorkflowRestController.java
@@ -6,6 +6,12 @@ import edu.coen241.workflowgen.model.WorkflowSpecResponse;
 import edu.coen241.workflowgen.repository.TaskInfoRepository;
 import edu.coen241.workflowgen.repository.WorkflowSpecRepository;
 import edu.coen241.workflowgen.service.TaskService;
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +24,7 @@ import java.util.Optional;
 
 @RestController
 public class WorkflowRestController {
+    public static final String DEFAULT_NAMESPACE = "default";
     @Autowired
     TaskInfoRepository taskInfoRepository;
     @Autowired
@@ -53,6 +60,69 @@ public class WorkflowRestController {
     public ResponseEntity<Void> deleteAllWorkflowSpecs() {
         workflowSpecRepository.deleteAll();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/workflowSpec/{workflowSpecId}/deploy")
+    public ResponseEntity<Void> deployWorkflowSpec(@PathVariable String workflowSpecId) {
+        //Optional<WorkflowSpecInfo> workflowSpecOp = workflowSpecRepository.findById(workflowSpecId);
+        try (KubernetesClient client = new DefaultKubernetesClient()) {
+            createDeployment(client);
+            createService(client);
+        }
+        catch (Exception e) {
+            // TODO: handle failed
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    private void createDeployment(KubernetesClient client) {
+        Deployment deployment = new DeploymentBuilder()
+                .withNewMetadata()
+                    .withName("workflow-ui-deployment")
+                    .addToLabels("app", "workflow-ui")
+                .endMetadata()
+                .withNewSpec()
+                    .withReplicas(1)
+                    .withNewSelector()
+                        .addToMatchLabels("app", "workflow-ui")
+                    .endSelector()
+                    .withNewTemplate()
+                        .withNewMetadata()
+                            .addToLabels("app", "workflow-ui")
+                        .endMetadata()
+                        .withNewSpec()
+                            .addNewContainer()
+                                .withName("workflow-ui")
+                                .withImage("nevin160/workflow-ui")
+                                .addNewPort()
+                                    .withContainerPort(8080)
+                                .endPort()
+                            .endContainer()
+                        .endSpec()
+                    .endTemplate()
+                .endSpec()
+                .build();
+        client.apps().deployments().inNamespace("default").createOrReplace(deployment);
+    }
+
+    private void createService(KubernetesClient client) {
+        Service service = new ServiceBuilder()
+                .withNewMetadata()
+                    .withName("workflow-ui-service")
+                    .addToLabels("app", "workflow-ui-service")
+                .endMetadata()
+                .withNewSpec()
+                    .withType("NodePort")
+                    .addNewPort()
+                        .withPort(8080)
+                        .withNodePort(30000)
+                    .endPort()
+                    .addToSelector("app", "workflow-ui")
+                .endSpec()
+                .build();
+
+        service = client.services().inNamespace(DEFAULT_NAMESPACE).create(service);
+
     }
 
 }
