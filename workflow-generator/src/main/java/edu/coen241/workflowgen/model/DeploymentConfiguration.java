d0f7368fc36e36c9aaea59bf3e4ebc2ddd2ca57d
Naveen Kumar <naveenkarya@github.com>
2022-05-18 11:49:57 -0700

    handle deployment status and nodeport

--- workflow-generator/src/main/java/edu/coen241/workflowgen/model/DeploymentConfiguration.java
+++ workflow-generator/src/main/java/edu/coen241/workflowgen/model/DeploymentConfiguration.java
@@ -1,12 +1,19 @@
 package edu.coen241.workflowgen.model;
 
+import io.fabric8.kubernetes.api.model.Quantity;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
+@Slf4j
 public class DeploymentConfiguration {
 
     private static final int DEFAULT_SERVICE_PORT = 8080;
@@ -19,12 +26,13 @@ public class DeploymentConfiguration {
     private int port;
     private String serviceType;
     private Integer nodePort;
+    private Map<String, Quantity> resourceLimits;
     public static DeploymentConfiguration SCHEDULER_CONFIG = buildSchedulerConfig();
     public static DeploymentConfiguration WORKFLOW_UI_CONFIG = buildWorkflowUIConfig();
 
     private static DeploymentConfiguration buildWorkflowUIConfig() {
 
-        System.out.println("workflow-ui env: " + System.getenv().get("workflow-ui-docker-image"));
+        log.info("workflow-ui docker image: " + System.getenv().get("workflow-ui-docker-image"));
         return DeploymentConfiguration.builder()
                 .dockerImage(System.getenv().getOrDefault("workflow-ui-docker-image", "nevin160/workflow-ui"))
                 .serviceName("workflow-ui-service")
@@ -37,7 +45,7 @@ public class DeploymentConfiguration {
     }
 
     private static DeploymentConfiguration buildSchedulerConfig() {
-        System.out.println("scheduler env: " + System.getenv().get("scheduler-service-docker-image"));
+        log.info("scheduler docker image: " + System.getenv().get("scheduler-service-docker-image"));
         return DeploymentConfiguration.builder()
                 .dockerImage(System.getenv().getOrDefault("scheduler-service-docker-image", "nevin160/scheduler-service"))
                 .serviceName("scheduler-service")
@@ -52,11 +60,31 @@ public class DeploymentConfiguration {
         DeploymentConfiguration deploymentConfiguration = DeploymentConfiguration.builder()
                 .dockerImage(taskInfo.getDockerImage())
                 .selector(taskInfo.getServiceName())
-                .serviceName(taskInfo.getServiceName() + "-service")
-                .deploymentName(taskInfo.getServiceName() + "-deployment")
+                .serviceName(taskInfo.getServiceName())
+                .deploymentName(taskInfo.getServiceName() + "-dep")
                 .port(DEFAULT_SERVICE_PORT)
                 .nodePort(taskInfo.getNodePort())
                 .build();
+        Map<String, Quantity> resourceLimits = new HashMap<>();
+        if(taskInfo.isCpuLimitNotEmpty()) {
+            try {
+                resourceLimits.put("cpu", Quantity.parse(taskInfo.getCpuLimit()));
+            }
+            catch (IllegalArgumentException ie) {
+                log.error(ie.getMessage());
+            }
+        }
+        if(taskInfo.isMemoryLimitNotEmpty()) {
+            try {
+                resourceLimits.put("memory", Quantity.parse(taskInfo.getMemoryLimit()));
+            }
+            catch (IllegalArgumentException ie) {
+                log.error(ie.getMessage());
+            }
+        }
+        if(resourceLimits.size() > 0) {
+            deploymentConfiguration.setResourceLimits(resourceLimits);
+        }
         if (taskInfo.getNodePort() != null) {
             deploymentConfiguration.setServiceType(NODE_PORT);
         } else {
