bf1a5c6a51be97b4af0d36d5ad753d513b922861
karthik-cm <karthikcm.77@gmail.com>
2022-05-22 14:58:22 -0700

    task failed status message changes

--- email-notification/src/main/java/com/wfaas/email/notification/dto/TaskDto.java
+++ email-notification/src/main/java/com/wfaas/email/notification/dto/TaskDto.java
@@ -7,15 +7,17 @@ public class TaskDto {
 	String taskId;
 	Map<String, String> attributes;
 	String status;
+	String statusMessage;
 	
 	public TaskDto() {}
 	
-	public TaskDto(String workflowId, String taskId, Map<String, String> attributes, String status) {
+	public TaskDto(String workflowId, String taskId, Map<String, String> attributes, String status, String statusMessage) {
 		super();
 		this.workflowId = workflowId;
 		this.taskId = taskId;
 		this.attributes = attributes;
 		this.status = status;
+		this.statusMessage = statusMessage;
 	}
 
 	public String getWorkflowId() {
@@ -41,7 +43,21 @@ public class TaskDto {
 	public void setAttributes(Map<String, String> attributes) {
 		this.attributes = attributes;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+	
 }
